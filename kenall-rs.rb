77272636c78a4a8b7889227c89d0891459c16fbd
chansuke <chansuke@georepublic.de>
2021-03-02 15:40:55 +0900

    Update for v0.1.3

--- kenall-rs.rb
+++ kenall-rs.rb
@@ -1,22 +1,24 @@
 class KenallRs < Formula
   desc "Unofficial command-line interface for Kenall API"
   homepage ""
-  url "https://github.com/chansuke/kenall-rs/archive/v0.1.0.tar.gz"
-  sha256 "f641ea591a3d70065f49c233c5ed47df9665d8312d553085035c8dacaf906534"
+  version "v0.1.3"
   license "MIT or Apache2"
 
-  version "0.1.0"
-  # depends_on "cmake" => :build
+
+  if OS.mac?
+    url "https://github.com/chansuke/kenall-rs/releases/download/#{version}/kenall-rs-macos.tar.gz"
+    sha256 "018171a1a7c834b26c319110023afdc3291abe5ae27833696b6376516f5ef52b"
+  elsif OS.linux?
+    url "https://github.com/chansuke/kenall-rs/releases/download/v0.1.3/kenall-rs-linux.tar.gz"
+    sha256 "d64807c9b886ab506b9f776f3dacb92a97e5adce9b44ffc2b937ef931cebb861"
+  end
+
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "kenall-rs"
   end
 
   test do
-    system "false"
+    system "#{bin}/kenall-rs", "--version"
   end
 end
