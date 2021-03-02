4c9a231116d6ac830efe1b7415f43f24b56ec174
chansuke <chansuke@georepublic.de>
2021-03-02 21:47:10 +0900

    fix

--- kenall-rs.rb
+++ kenall-rs.rb
@@ -4,15 +4,13 @@ class KenallRs < Formula
   version "v0.1.3"
   license "MIT or Apache2"
 
-
   if OS.mac?
     url "https://github.com/chansuke/kenall-rs/releases/download/#{version}/kenall-rs-macos.tar.gz"
     sha256 "018171a1a7c834b26c319110023afdc3291abe5ae27833696b6376516f5ef52b"
   elsif OS.linux?
-    url "https://github.com/chansuke/kenall-rs/releases/download/v0.1.3/kenall-rs-linux.tar.gz"
+    url "https://github.com/chansuke/kenall-rs/releases/download/#{version}/kenall-rs-linux.tar.gz"
     sha256 "d64807c9b886ab506b9f776f3dacb92a97e5adce9b44ffc2b937ef931cebb861"
   end
-
 
   def install
     bin.install "kenall-rs"
